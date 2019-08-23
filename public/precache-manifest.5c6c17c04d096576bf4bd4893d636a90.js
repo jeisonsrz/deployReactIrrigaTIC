1a02abb89fb8db656371a9017eb63988c396363a
jeisonsrz <jeison.ruiz.z@uniautonoma.edu.co>
2019-08-23 13:42:10 -0500

    🌱commit deploy IrrigaTIC - agregando build Frontend🌱

---
+++ public/precache-manifest.5c6c17c04d096576bf4bd4893d636a90.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "afbc09dd6dc2f10f1967",
+    "url": "/material-dashboard-react/static/css/main.942db858.chunk.css"
+  },
+  {
+    "revision": "b8556c08b70d6c956861",
+    "url": "/material-dashboard-react/static/js/runtime~main.b8556c08.js"
+  },
+  {
+    "revision": "77f754d2f29910b6c7bc46fa23ad5e0a",
+    "url": "/material-dashboard-react/static/media/logo1.77f754d2.jpeg"
+  },
+  {
+    "revision": "0c7d93b4e05977a0ca78",
+    "url": "/material-dashboard-react/static/js/2.0c7d93b4.chunk.js"
+  },
+  {
+    "revision": "afbc09dd6dc2f10f1967",
+    "url": "/material-dashboard-react/static/js/main.afbc09dd.chunk.js"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/material-dashboard-react/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/material-dashboard-react/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/material-dashboard-react/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "0c7d93b4e05977a0ca78",
+    "url": "/material-dashboard-react/static/css/2.15893d3b.chunk.css"
+  },
+  {
+    "revision": "4be1087d7b611ceeb1c73c5db3268545",
+    "url": "/material-dashboard-react/index.html"
+  }
+];
---
+++ public/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/material-dashboard-react/precache-manifest.5c6c17c04d096576bf4bd4893d636a90.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/material-dashboard-react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
