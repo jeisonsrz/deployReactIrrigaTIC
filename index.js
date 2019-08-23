5b184338b7e64ad05cf902bfbb90373dbc044883
jeisonsrz <jeison.ruiz.z@uniautonoma.edu.co>
2019-08-23 13:28:55 -0500

    🌱commit deploy IrrigaTIC 🌱

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors({
 }))
 
 app.use((req,res,next) =>{
-res.header("Access-Control-Allow-Origin","*0");
+res.header("Access-Control-Allow-Origin","*");
 res.header(
     "Access-Control-Alow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
