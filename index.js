839c0788c7138f6c0b51a8d9e3506c73b94b45ba
jeisonsrz <jeison.ruiz.z@uniautonoma.edu.co>
2019-08-26 00:36:14 -0500

    Commit error Static

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ next();
 });
 app.use(express.static("."));
 //app.use("/", express.static(__dirname + "/public"));
-app.use("/",express.static(__dirname + 'public'));
-//app.use(express.static(path.join(__dirname,"public")));
+//app.use("/",express.static(__dirname + 'public'));
+app.use(express.static(path.join(__dirname,"public")));
 app.use('/assets', express.static(path.join(__dirname, "../assets")));
 
 
