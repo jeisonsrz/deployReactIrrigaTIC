3a7191f8dac86b0b2777d42fffdfa233cc505ba3
jeisonsrz <jeison.ruiz.z@uniautonoma.edu.co>
2019-09-09 15:31:18 -0500

    commit azure final expo

---
+++ public/precache-manifest.97e034157eef2a858132268b1057a1f0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/material-dashboard-react/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "b16715fbfb037378e001",
+    "url": "/material-dashboard-react/static/css/main.495832ad.chunk.css"
+  },
+  {
+    "revision": "b8556c08b70d6c956861",
+    "url": "/material-dashboard-react/static/js/runtime~main.b8556c08.js"
+  },
+  {
+    "revision": "432755b32f2a4d212060ea593b4cbf4f",
+    "url": "/material-dashboard-react/static/media/automatizacion.432755b3.jpg"
+  },
+  {
+    "revision": "a89114c4f67c701829fb",
+    "url": "/material-dashboard-react/static/js/2.a89114c4.chunk.js"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/material-dashboard-react/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/material-dashboard-react/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "b16715fbfb037378e001",
+    "url": "/material-dashboard-react/static/js/main.b16715fb.chunk.js"
+  },
+  {
+    "revision": "c3c07d3d9b4099acb3143388f2b2cfee",
+    "url": "/material-dashboard-react/static/media/fondo4.c3c07d3d.jpg"
+  },
+  {
+    "revision": "71a68088184109ea747f8d364461df24",
+    "url": "/material-dashboard-react/static/media/3dprint.71a68088.jpg"
+  },
+  {
+    "revision": "aaa80ead1401e98a5275f367d6b494c5",
+    "url": "/material-dashboard-react/static/media/hardware.aaa80ead.jpg"
+  },
+  {
+    "revision": "73b5ab1379c2f51118b5078ce5cda061",
+    "url": "/material-dashboard-react/static/media/ecommerce.73b5ab13.jpg"
+  },
+  {
+    "revision": "77f754d2f29910b6c7bc46fa23ad5e0a",
+    "url": "/material-dashboard-react/static/media/logo1.77f754d2.jpeg"
+  },
+  {
+    "revision": "a89114c4f67c701829fb",
+    "url": "/material-dashboard-react/static/css/2.60da6b73.chunk.css"
+  },
+  {
+    "revision": "61c4b277d3ce5166d233b235043ba717",
+    "url": "/material-dashboard-react/index.html"
+  }
+];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/material-dashboard-react/precache-manifest.6c26371cb0bd3aa785361c70ad072e13.js"
+  "/material-dashboard-react/precache-manifest.97e034157eef2a858132268b1057a1f0.js"
 );
 
 workbox.clientsClaim();
